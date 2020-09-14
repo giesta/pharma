4d917f18e8dc433dc047c2d760d40c31a54a9a88
SC\giesta9 <giedriustan@gmail.com>
2020-09-15 00:30:42 +0300

    added many to many relation for drugs and diseases

--- backend/app/Models/Disease.php
+++ backend/app/Models/Disease.php
@@ -9,6 +9,19 @@ class Disease extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name', 'description', 'symptoms',
+        'name', 'description', 'symptoms', 'treatment_id',
     ];
+
+    public function treatments(){
+        return $this->hasMany(Treatment::class,'treatment_id', 'id');
+    }
+    public function drugs()
+{
+   //return $this->belongsToMany(RelatedModel, pivot_table_name, foreign_key_of_current_model_in_pivot_table, foreign_key_of_other_model_in_pivot_table);
+   return $this->belongsToMany(
+        Drug::class,
+        'diseases_drugs',
+        'disease_id',
+        'drug_id');
+}
 }
--- backend/app/Models/Drug.php
+++ backend/app/Models/Drug.php
@@ -11,4 +11,13 @@ class Drug extends Model
     protected $fillable = [
         'name', 'substance', 'indication', 'contraindication', 'reaction', 'use',
     ];
+    public function diseases()
+    {
+        //return $this->belongsToMany(RelatedModel, pivot_table_name, foreign_key_of_current_model_in_pivot_table, foreign_key_of_other_model_in_pivot_table);
+        return $this->belongsToMany(
+            Disease::class,
+            'diseases_drugs',
+            'drug_id',
+            'disease_id');
+    }
 }

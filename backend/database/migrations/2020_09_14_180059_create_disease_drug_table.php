6f9dac82f06b3d49511a954baf760f4ff74925e4
SC\giesta9 <giedriustan@gmail.com>
2020-09-15 16:34:37 +0300

    changed name of table

--- backend/database/migrations/2020_09_14_180059_create_disease_drug_table.php
+++ backend/database/migrations/2020_09_14_180059_create_disease_drug_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDiseasesDrugsTable extends Migration
+class CreateDiseaseDrugTable extends Migration
 {
     /**
      * Run the migrations.

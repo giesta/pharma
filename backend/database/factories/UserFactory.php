5c74e241ef5746f4c98bc2a55bf3d24622fa2828
SC\giesta9 <giedriustan@gmail.com>
2020-09-24 23:41:32 +0300

    added seeder for user's admin

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,9 +24,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'email' => 'a@a.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$no1oQTjlEGGl0syZga85CuDE/lvlJQ2Si3mftIAanl6BiZiWAm0r2', // password
+            'role' => 'admin',
             'remember_token' => Str::random(10),
         ];
     }

eb09381809b83813fd8a1f1cf5e7360966728262
SC\giesta9 <giedriustan@gmail.com>
2020-10-01 08:51:34 +0300

    fix for services

--- ui-app/src/services/auth.service.js
+++ ui-app/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/auth/";
+const API_URL = "http://20.52.35.177:5000/api/auth/";
 
 class AuthService {
   login(email, password) {

247a68814b0de027e8240db3d8969f4cae200bf3
SC\giesta9 <giedriustan@gmail.com>
2020-11-13 22:38:26 +0200

    added app url

--- backend/app/Http/Resources/Treatment.php
+++ backend/app/Http/Resources/Treatment.php
@@ -20,7 +20,7 @@ class Treatment extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
-            'algorithm' => env('APP_URL').Storage::url($this->algorithm),
+            'algorithm' => config('app.url').Storage::url($this->algorithm),
             'disease' => new DiseaseResource($this->disease),
         ];
     }
